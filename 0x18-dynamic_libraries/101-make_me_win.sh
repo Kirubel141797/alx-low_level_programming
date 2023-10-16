20829cbba5f6345fb39c13592bf84a46dba9fabd
kirubel141797 <kirubelteshome22@gmail.com>
2023-10-16 10:58:24 -0700

    all in one

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/kirubel141797/alx-low_level_programming/raw/master/0x18-dybamic_libraries/libgiga.so
+wget -p /tmp https://github.com/kirubel141797/alx-low_level_programing/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
