44ad21a5825fb35d1dc4e4ad15f3b649f9b73eb3
kirubel141797 <kirubelteshome22@gmail.com>
2023-10-16 11:35:14 -0700

    why always me

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p /tmp https://github.com/sadatmisr/alx-low_level_programing/raw/master/0x18-dynamic_libraries/libgiga.so
+wget -P /tmp https://github.com/kirubel141797/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD=/tmp/libgiga.so
