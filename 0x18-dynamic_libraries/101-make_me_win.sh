b71e92ec870c441105a61de9b6560fa3b29be02e
kirubel141797 <kirubelteshome22@gmail.com>
2023-10-16 05:50:04 -0700

    my 4th

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,3 @@
-export LD_PRELOAD=$PWD/winning_lib.so
+echo -n "base64 myrand.so > myrand_encoded.txt" | base64 -d > ./myrand.so
+export LD_PRELOAD=$PWD/myrand.so
 sleep 98
---
+++ 0x18-dynamic_libraries/myrand.c
@@ -0,0 +1,5 @@
+int rand(void) {
+    static int i;
+    int arr[6] = {9, 8, 10, 24, 75, 9};
+    return arr[i++ % 6];
+}
